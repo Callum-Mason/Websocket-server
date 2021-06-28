91a76da852018a698a510b53f99aa9d5d920a055
Callum Mason <callum@mason.co.uk>
2021-06-28 17:33:57 +0100

    added Server broadcast

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { Server } from "ws";
 
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const ip = "127.0.0.1";
 
 const port = 5757
+
+const url = "ws://" + ip + ":" + port;
 
 
 const wss = new Server({ port: port });
@@ -16,7 +25,7 @@ const wss = new Server({ port: port });
 
     // Send a message to the client on connection
 
-    ws.send("Connected...");
+    ws.send("");
   });
 
 
@@ -24,16 +33,46 @@ const wss = new Server({ port: port });
 function messagetobedecoded(messageReceived: any) {
   console.log(messageReceived);
 
-  // messagetobeencoded(messageReceived);
+  
 }
 
-// function messagetobeencoded(sendmessage: any) {
-//   const ws = new WebSocket("ws://"+ip+port);
+
+
+
+function messagetobeencoded(sendmessage: any) {
+  const WebSocket = require("ws");
+
+  const ws = new WebSocket(url);
+  console.log(url);
+
+  ws.on("open", function open() {
+    ws.send(sendmessage);
+  });
+
   
-//   ws.send(sendmessage)
+  
 
-// }
+}
 
+
+function messagetobebroadcast(message: any) {
+
+
+  const WebSocket = require("ws");
+
+  const wss = new WebSocket(url);
+  // console.log(url);
+wss.on("connection", function connection(ws: any) {
+  ws.on("message", function incoming() {
+    wss.clients.forEach(function each(client: any) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+}
 
 
 
@@ -41,5 +80,21 @@ function messagetobedecoded(messageReceived: any) {
 (async () => {
   // const url = await connect(5757);
   // console.log(url);
-  console.log("ws://" + ip + port);
+  console.log(url);
+
+  rl.question("What is your name ? ", function saveInput(name: any) {
+    console.log(`His name is ${name}`);
+    messagetobeencoded("hello World");
+
+    messagetobebroadcast(name);
+    rl.close();
+  });
+
+  
+
+  
+
+
+  
 })();
+
